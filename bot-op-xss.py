f9db3e3a09eae826640d35ac9452c1622e70ccf9
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-04-12 15:58:18 -0300

    Update bot-op-xss.py

--- bot-op-xss.py
+++ bot-op-xss.py
@@ -26,6 +26,11 @@ for url in lista:
     #Executa o SlicePathsURL pra cortar os URLS
     os.system("cat "+domain+"/all_domain-gauplus.txt "+domain+"/katana-tmp.txt | grep -Ev ':80|embed'|slicepathsurl |grep -Ev '4|5|6|7|8|9' > "+domain+"/urls_slice.txt")
 
+    if url.startswith("https://"):
+        url = url.replace("https://", "", 1)
+    elif url.startswith("http://"):
+        url = url.replace("http://", "", 1)
+
     #Open Redirect
     os.system("cat "+domain+"/urls_slice.txt | nuclei -t /root/nuclei-templates/cves/2022/CVE-2022-28923.yaml -t /root/nuclei-templates/vulnerabilities/generic/open-redirect.yaml -c 75 > "+domain+"/"+url+"_openredirect.txt")
 
