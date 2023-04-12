740ddc251b7fd0c9608836222a528478843a7ac4
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-04-12 15:43:02 -0300

    Create bot-op-xss.py

---
+++ bot-op-xss.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+domain = "all_domain"
+
+parser.add_argument("-l", "--lista", help="Caminho da Lista")
+args = parser.parse_args()
+path = args.lista
+
+# Lê o arquivo e armazena cada linha em uma lista
+with open(path, "r") as file:
+    lista = file.readlines()
+
+# Imprime cada elemento da lista em um loop
+for url in lista:
+    os.system("mkdir "+domain)
+
+    os.system("subfinder -d  "+url+" | httpx > "+domain+"/subf-tmp.txt")
+
+    os.system("cat "+domain+"/subf-tmp.txt | gauplus > "+domain+"/all_domain-gauplus.txt")
+
+    os.system("cat "+domain+"/subf-tmp.txt |katana > "+domain+"/katana-tmp.txt") 
+
+    #Executa o SlicePathsURL pra cortar os URLS
+    os.system("cat "+domain+"/all_domain-gauplus.txt "+domain+"/katana-tmp.txt | grep -Ev ':80|embed'|slicepathsurl |grep -Ev '4|5|6|7|8|9' > "+domain+"/urls_slice.txt")
+
+    #Open Redirect
+    os.system("cat "+domain+"/urls_slice.txt | nuclei -t /root/nuclei-templates/cves/2022/CVE-2022-28923.yaml -t /root/nuclei-templates/vulnerabilities/generic/open-redirect.yaml -c 75 > "+domain+"/"+url+"_openredirect.txt")
+
+    #Git e CRLF Injection
+    os.system("cat "+domain+"/urls_slice.txt | nuclei -tags git,crlf -c 75 > "+domain+"/"+url+"_git-crlf.txt")
+
+    #Swagger XSS
+    os.system("cat "+domain+"/urls_slice.txt | nuclei -t  multiples-swagger-xss-indentify.yaml -c 75 > "+domain+"/"+url+"_swagger-xss.txt")
