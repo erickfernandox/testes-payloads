dd9bd729fba7ad9fda011b5ff5bd9bb64acc29b9
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-04-12 14:26:26 -0300

    Update bot-vul.py

--- bot-vul.py
+++ bot-vul.py
@@ -10,14 +10,26 @@ os.system("cat "+domain+"/subf-tmp.txt | gauplus > "+domain+"/all_domain-gauplus
 
 os.system("cat "+domain+"/subf-tmp.txt |katana > "+domain+"/katana-tmp.txt") 
 
+#Executa o SlicePathsURL pra cortar os URLS
 os.system("cat "+domain+"/all_domain-gauplus.txt "+domain+"/katana-tmp.txt | grep -Ev ':80|embed'|slicepathsurl |grep -Ev '4|5|6|7|8|9' > "+domain+"/urls_slice.txt")
 
-os.system("cat "+domain+"/urls_slice.txt | nuclei -t /root/nuclei-templates/cves/2022/CVE-2022-28923.yaml -t /root/nuclei-templates/vulnerabilities/generic/open-redirect.yaml > "+domain+"/openredirect.txt")
+#Open Redirect
+os.system("cat "+domain+"/urls_slice.txt | nuclei -t /root/nuclei-templates/cves/2022/CVE-2022-28923.yaml -t /root/nuclei-templates/vulnerabilities/generic/open-redirect.yaml -c 75 > "+domain+"/openredirect.txt")
 
-os.system("cat "+domain+"/urls_slice.txt | nuclei -tags git,crlf > "+domain+"/git-crlf.txt")
+#Git e CRLF Injection
+os.system("cat "+domain+"/urls_slice.txt | nuclei -tags git,crlf -c 75 > "+domain+"/git-crlf.txt")
 
-os.system("cat "+domain+"/urls_slice.txt | nuclei -t  multiples-swagger-xss-indentify.yaml > "+domain+"/swagger-xss.txt")
+#Swagger XSS
+os.system("cat "+domain+"/urls_slice.txt | nuclei -t  multiples-swagger-xss-indentify.yaml -c 75 > "+domain+"/swagger-xss.txt")
 
+#Pasta Exposta
 os.system("cat "+domain+"/urls_slice.txt | httpx -title | grep 'Index of' > "+domain+"/indexof.txt")
 
-os.system("cat "+domain+"/urls_slice.txt | nuclei -tags traversal > "+domain+"/traversal.txt")
+#Path Traversal
+os.system("cat "+domain+"/urls_slice.txt | nuclei -tags traversal -c 75 > "+domain+"/traversal.txt")
+
+#Informações Expostas
+os.system("cat "+domain+"/urls_slice.txt | nuclei -t /root/nuclei-templates/exposures/configs/ -t /root/nuclei-templates/exposures/logs/  -t /root/nuclei-templates/exposures/apis/ -t /root/nuclei-templates/exposures/backups/ -c 75 > "+domain+"/exposures.txt")
+
+#Ultimos CVES 2022 e 2023
+os.system("cat "+domain+"/urls_slice.txt | nuclei -t /root/nuclei-templates/cves/2022/ -t /root/nuclei-templates/cves/2023/ -c 75 > "+domain+"/ultimos-cves.txt")
