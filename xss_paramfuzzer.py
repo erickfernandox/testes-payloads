17fa0cc9c7beda2813a28a6ca295faf9eb43c0a2
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-06-27 23:10:53 -0300

    Update xss_paramfuzzer.py

--- xss_paramfuzzer.py
+++ xss_paramfuzzer.py
@@ -9,9 +9,12 @@ urls = args.lista
 
 payload_xss_1 = "?q=XSS&search=XSS&query=XSS&keywords=XSS&term=XSS&text=XSS&input=XSS&value=XSS&username=XSS&password=XSS&name=XSS&email=XSS&comment=XSS&message=XSS&body=XSS&title=XSS&subject=XSS&description=XSS&content=XSS&id=XSS&user=XSS&uid=XSS&pid=XSS&eid=XSS&cid=XSS&gid=XSS&aid=XSS&tid=XSS&wid=XSS&lid=XSS&mid=XSS&fid=XSS&did=XSS&oid=XSS&vid=XSS&sid=XSS&rid=XSS&file=XSS&msg=XSS"
 payload_xss_2 = "?redirect=XSS&return=XSS&url=XSS&link=XSS&next=XSS&callback=XSS&referrer=XSS&referer=XSS&target=XSS&destination=XSS&returnurl=XSS&return_uri=XSS&return_path=XSS&returnto=XSS&return_to=XSS&page=XSS&view=XSS&section=XSS&module=XSS&component=XSS&task=XSS&action=XSS&option=XSS&format=XSS&tmpl=XSS&layout=XSS&lang=XSS&language=XSS&charset=XSS&stylesheet=XSS&script=XSS&js=XSS"
+payload_xss_3 = "?category=XSS&filter=XSS&sort=XSS&order=XSS&limit=XSS&offset=XSS&page_size=XSS&search_term=XSS&query_string=XSS&search_query=XSS&search_param=XSS&search_filter=XSS&search_sort=XSS&search_order=XSS&search_limit=XSS&search_offset=XSS&search_page_size=XSS&field=XSS&column=XSS&row=XSS&value=XSS
 
 execucao_xss_1 = "cat "+urls+"| urlfusion '/"+payload_xss_1+"' |airixss -payload '\"XSS' | grep -Ev 'Not'"
 execucao_xss_2 = "cat "+urls+"| urlfusion '/"+payload_xss_2+"' |airixss -payload '\"XSS' | grep -Ev 'Not'"
+execucao_xss_3 = "cat "+urls+"| urlfusion '/"+payload_xss_2+"' |airixss -payload '\"XSS' | grep -Ev 'Not'"
 
 os.system(execucao_xss_1)
 os.system(execucao_xss_2)
+os.system(execucao_xss_3)
