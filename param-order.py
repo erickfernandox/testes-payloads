1142e21333f9f0754eb1f27a64ee534436c823b2
Erick Fernando <erickfernandoxavier@hotmail.com>
2023-07-07 14:58:57 -0300

    Create param-order.py

---
+++ param-order.py
@@ -0,0 +1,31 @@
+import argparse
+
+def format_parameters(lst):
+    params = []
+    line = ""
+    for item in lst:
+        param = f"{item}=FUZZ"
+        if len(param) > 25:
+            params.append(line)
+            line = ""
+        if line:
+            line += "&"
+        line += param
+    if line:
+        params.append(line)
+    return params
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--listfile", help="File containing the list", required=True)
+    args = parser.parse_args()
+
+    with open(args.listfile, "r") as file:
+        lst = file.read().splitlines()
+
+    params = format_parameters(lst)
+    for param in params:
+        print(f"?{param}")
+
+if __name__ == "__main__":
+    main()
